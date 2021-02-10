e0598b5727ed76645b42f0d60e3712e8a1993359
Max Fechner <max.fechner@gmail.com>
2021-02-11 00:03:12 +0100

    Add files via upload

---
+++ xml.test.js
@@ -0,0 +1,18 @@
+import XML from './xml.js';
+
+
+let c = XML.parse(`<pat>a<more b="c"> c
+dd</more>aa<test a="1" b='2 4'
+c=' dd d'/> <ns:x><y>z</y></ns:x></pat>`)
+console.log(JSON.stringify(c, 0, 4))
+
+// [
+// 	{tag:'pat', children:[
+// 		'a',
+// 		{tag:'more', attributes:{b:'c'}, children:[
+// 			' cdd'
+// 		]},
+// 		'aa',
+// 		{tag:'test', attributes:{a:'1',b:'2'}}
+// 	]}
+// ]
